99cffb942538e0b856b8096247a0569c3ea66a82
Immo Heikkinen <immo.heikkinen@gmail.com>
2015-03-06 10:47:35 +0200

    Bump cljs & cljsbuild versions
    cljs compiler requires clj 1.6.0

--- project.clj
+++ project.clj
@@ -4,13 +4,15 @@
   :url "https://github.com/immoh/dommy.template"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :plugins [[lein-cljsbuild "0.3.2"]
+  :plugins [[lein-cljsbuild "1.0.5"]
             [com.cemerick/clojurescript.test "0.2.1"]
             [com.cemerick/austin "0.1.3"]]
 
   :hooks [leiningen.cljsbuild]
 
-  :profiles {:dev {:dependencies [[crate "0.2.3"] ;; for perf test
+  :profiles {:dev {:dependencies [[org.clojure/clojure "1.6.0"]
+                                  [org.clojure/clojurescript "0.0-2913"]
+                                  [crate "0.2.3"] ;; for perf test
                                   [com.cemerick/clojurescript.test "0.2.1"]]}}
 
   :lein-release {:deploy-via :shell
